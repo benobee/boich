f02e45ce4c1a7fc2f940dc50af7a02c23bb4c03b
Ben Obee <benobee@gmail.com>
2017-09-13 16:19:37 -0700

    module reconfig, adding landing screen module

---
+++ src/modules/landing.js
@@ -0,0 +1,77 @@
+import $ from "jquery";
+
+/* This module is used to contorl the behaviour of
+the first initial full page branding screen. It is triggered
+once per session and only on the home where the code lives in the 
+home page collection header injection. */
+
+//basic session storage module 
+const Session = {
+    set(key, value) {
+        sessionStorage.setItem(key, JSON.stringify(value));
+    },
+    get(key) {
+        const item = sessionStorage.getItem(key);
+
+        return JSON.parse(item);
+    }
+};
+
+const Landing = {
+    init() {
+        this.sessionState();
+        this.cacheDOM();
+
+        if (this.isFirstSession) {
+            this.branding.classList.add("active");
+            this.body.classList.add("branding-landing-screen");
+            this.animations();
+            this.detect();
+            this.events();
+        }
+    },
+    animations() {
+        this.branding.classList.add("SMIL");
+    },
+    detect() {
+        // Internet Explorer 6-11
+        const isIE = /*@cc_on!@*/ false || !!document.documentMode;
+
+        // Edge 20+
+        const isEdge = !isIE && !!window.StyleMedia;
+
+        if (!(isIE || isEdge)) {
+            this.branding.classList.add("compatible");
+        } else {
+            this.branding.classList.add("incompatible");
+        }
+    },
+    sessionState() {
+        this.isFirstSession = false;
+
+        const state = Session.get("first-session-visite");
+
+        if (state === null) {
+            this.isFirstSession = true;
+        }
+    },
+    cacheDOM() {
+        this.body = document.querySelector("body");
+        this.button = this.body.querySelector(".landing-screen .button");
+        this.branding = this.body.querySelector(".landing-screen");
+    },
+    events() {
+        $(this.button).on("click", () => {
+            this.branding.classList.add("transition-out");
+
+            setTimeout(() => {
+                this.body.classList.remove("branding-landing-screen");
+                this.branding.classList.remove("active");
+            }, 1500);
+
+            Session.set("first-session-visite", false);
+        });
+    }
+};
+
+export default Landing;
